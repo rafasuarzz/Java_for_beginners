b6009f14a2eddd6b6208494f769f48f97cd21c17
Rafa <rafasuarzz@gmail.com>
2023-09-24 12:19:52 +0100

    Last commit

--- 5. ArrayList and 2D Arrays/2D Arrays/5.3 Chessboard/src/Chessboard.java
+++ 5. ArrayList and 2D Arrays/2D Arrays/5.3 Chessboard/src/Chessboard.java
@@ -3,7 +3,6 @@ public class Chessboard
    public static void main(String[] args)
    {
       char[][] chessboard = new char[8][8];
-
       for(int i = 0; i < 8; i++){
          for(int j = 0; j < 8; j++){
             if ((i + j) % 2 == 0){
